bdad63204e440b1b499dd72eb54ef465ca8e5aba
Takeshi Nakano <tf0054@gmail.com>
2014-11-03 13:26:38 +0100

    Added function for parsing cmd line args.

--- src/aolsender/core.clj
+++ src/aolsender/core.clj
@@ -1,20 +1,51 @@
 (ns aolsender.core
+  (:use [clojure.pprint])
   (:require
    [aolsender.http :as http]
    [throttler.core :refer [throttle-chan]]
+   [clojure.string :as string]
+   [clojure.tools.cli :refer [parse-opts]]
    [clojure.core.async :refer [chan >!! <!! thread]]
    ))
 
-; ---
-(def persec 3)
-(def userid "544a65950cf28a00f105fb79")
-(def tuplename "queryTuple")
-(def hosturl "192.168.30.10:9191")
-; ---
+(def cli-options
+  ;definitions of option
+  ;long option should have an exampleo in it....
+  [["-t" "--tuple A" "REQUIRED: Target tuple name you want to send"
+    :id :tuplename
+    :default nil]
+   ["-u" "--userid A" "REQUIRED: Target user name you want to send"
+    :id :userid
+    :default nil]
+   ["-s" "--server localhost:2181" "gungnir server address"
+    :id :hosturl
+    :default "internal-vagrant.genn.ai:9191"]
+   ["-l" "--limit 10" "Reset per millsec"
+    :id :persec
+    :default 10]
+   ["-n" "--numthread 8" "Num of Sending threads"
+    :id :numthread
+    :default 8]
+   ["-h" "--help" "Show this help msg"]])
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn usage [options-summary]
+  (->> ["This is my program. There are many like it, but this one is mine."
+        ""
+        "Usage: program-name [options] action"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Please refer to the manual page for more information."]
+       (string/join \newline)))
 
 (def in (chan 1))
-(def slow-chan (throttle-chan in persec :second))
-;(def slow-chan (throttle-chan in 1 :millisecond))
+;(def slow-chan (throttle-chan in persec :persec))
+;(def slow-chan (throttle-chan in 1 ))
 
 (def not-nil? (complement nil?))
 
@@ -31,27 +62,40 @@
 
 (defn postEachLine
   "Post the given data with http-kit client."
-  [line]
+  [options line]
   (http/postItem
-   hosturl userid tuplename line
+   (:hosturl options) (:userid options) (:tuplename options) line
    (partial showResponse {:line line
                           :threadid (showThreadId)
                           :start (System/currentTimeMillis)})))
 
 (defn async-kicker
   "Start num-consumers threads that will consume work from the slow-chan"
-  [num-consumers]
-  (dotimes [_ num-consumers]
-    (thread
-      (while true
-        (let [line (<!! slow-chan)]
-          (postEachLine (clojure.string/trim-newline line)))))))
+  [options]
+  (let [slow-chan (throttle-chan in (:persec options) :second)]
+    (dotimes [_ (:numthread options)]
+      (thread
+        (while true
+          (let [line (<!! slow-chan)]
+            (postEachLine options (clojure.string/trim-newline line))))))))
 
 (defn -main
   "Main function called via 'lein run'"
   [& args]
-  (println (str "aolsender started with " persec " threads."))
-  (do
-    (async-kicker 8)
-    (doseq [line (line-seq (java.io.BufferedReader. *in*))]
-      (>!! in line))))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (if (:help options)
+      (exit 0 (usage summary)))
+    (if (nil? (:tuplename options))
+      (do (println "Tuple name have to be defined using \"-t\" option")
+        (exit 0 (usage summary)))
+      (println "Tuple: " (:tuplename options)))
+    (if (nil? (:userid options))
+      (do (println "Userid have to be defined using \"-t\" option")
+        (exit 0 (usage summary)))
+      (println "Userid: " (:userid options)))
+    (println (str "aolsender started with " (:persec options) " per sec limit."))
+    ;(pprint options)(exit 1 "DEBuG")
+    (do
+      (async-kicker options)
+      (doseq [line (line-seq (java.io.BufferedReader. *in*))]
+        (>!! in line)))))
